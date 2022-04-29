e8853d2dc16093eeec76f4ce55be1c508849a44f
Cooper Trowbridge <ctrowbri@redhat.com>
2022-04-29 15:05:41 -0600

    generate sections and tabs

--- scripts/generated.sidenav.js
+++ scripts/generated.sidenav.js
@@ -1,11 +1,16 @@
 const generatedRoutes = require(require('path').join(process.cwd(), 'src/generated'));
 const sideNav = [];
+const sections = [];
 
 Object.entries(generatedRoutes).forEach(([key]) => {
-  const text = key.split('/')[1];
+  let section = key.split('/')[1];
+  section = section.charAt(0).toUpperCase() + section.slice(1);
+  !sections.includes(section) && sections.push(section);
+});
+
+sections.forEach(sectionName => {
   sideNav.push({
-    text,
-    href: key
+    section: sectionName
   });
 });
 

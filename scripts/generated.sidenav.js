0c53c2674de1d6f4e387452c1659b9ce435eb14b
Cooper Trowbridge <ctrowbri@redhat.com>
2022-05-03 15:38:00 -0600

    dont push section if already exists

--- scripts/generated.sidenav.js
+++ scripts/generated.sidenav.js
@@ -1,12 +1,14 @@
 const generatedRoutes = require(require('path').join(_PF_DOCS_PACKAGE_PWD, 'src/generated'));
 const sideNav = [];
-const sections = [];
 
 Object.entries(generatedRoutes).forEach(([key, { section }]) => {
   const nav = {};
   const text = key.split('/')[1];
 
   if (section && section !== '') {
+    if (sideNav.find(({ section: cur }) => cur === section)) {
+      return;
+    }
     nav.section = section;
   } else {
     nav.text = text.charAt(0).toUpperCase() + text.slice(1);
